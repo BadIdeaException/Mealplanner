b7091aad51689825dc5321f2e5f9ca6272cbcd85
chris <tigerman33@gmx.net>
2022-01-19 21:21:51 +0100

    Include search actions for Half Baked Harvest and Damn Delicious

--- suggest.js
+++ suggest.js
@@ -17,15 +17,28 @@ const CATEGORIES = [
 function init() {
 	const CONTENT_PANE = $('#content-pane');
 
-	for (i = 0; i < CATEGORIES.length; i++) {
+	for (category of CATEGORIES) {
 		// Build the category card
-		var card = $('<div id="card-' + i + '" class="card" data-caption="' + CATEGORIES[i] + '">' + CATEGORIES[i] + '</div>');
+		let card = $('<div class="card" data-caption="' + category + '">' + category + '</div>');
 
-		var link = $('<a href="#">Search on <img src="https://www.pinterest.com/favicon.ico"/> Pinterest</a>')
-		link.bind('click', searchPinterest);
-		$('<div class="actions"></div>')
-			.append(link)
-			.appendTo(card);
+		let actionPane = $('<div class="actions"></div>');
+		card.append(actionPane);
+		[ 
+			{ name: 'Pinterest', icon: 'https://www.pinterest.com/favicon.ico', baseURL: 'https://www.pinterest.com/search/my_pins/?q=', suffix: ' recipes' },
+			{ name: 'Damn Delicious', icon: 'https://s23209.pcdn.co/wp-content/themes/damndelicious2021/favicon/favicon-32x32.png', baseURL: 'https://damndelicious.net/?s=', suffix: '' },
+			{ name: 'Half Baked Harvest', icon: 'https://www.halfbakedharvest.com/wp-content/uploads/2021/10/cropped-favicon-32x32.png', baseURL: 'https://www.halfbakedharvest.com/?s=', suffix: '' }
+		].forEach(action => {
+			let link = $('<a href="#">Search on <img src="' + action.icon + '"/> ' + action.name + '</a>');
+			link.bind('click', function search(e) {			
+				console.log($(e.target).closest('.card'));
+				window.open(
+					action.baseURL + 
+					encodeURIComponent($(e.target).closest('.card').attr('data-caption') + 
+					action.suffix)
+				);
+			});
+			actionPane.append(link);
+		});
 		card.appendTo(CONTENT_PANE);
 	}
 	suggest();
@@ -37,17 +50,6 @@ function suggest() {
 
 	// Activate a random one by number
 	$('#card-' + Math.floor(CATEGORIES.length * Math.random())).addClass('active');
-}
-
-function searchPinterest(e) {
-	//Reverse engineered url to search own pins on Pinterest
-	const BASE_URL = 'https://www.pinterest.com/search/my_pins/?q=';
-	
-	// Get surrounding card element
-	var el = $(e.target).closest('.card');
-	var searchterm = el.attr('data-caption') + ' recipes';
-	
-	window.open(BASE_URL + encodeURIComponent(searchterm));
 }
 
 $(document).ready(init);
